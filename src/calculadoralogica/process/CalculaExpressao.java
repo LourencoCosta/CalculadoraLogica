93fe4c461a71f627edae66ea00af91abc9e59c5e
rlc <rafaelconteudossi@gmail.com>
2018-03-27 22:40:07 -0300

    Correcao Final

--- src/calculadoralogica/process/CalculaExpressao.java
+++ src/calculadoralogica/process/CalculaExpressao.java
@@ -15,56 +15,53 @@ import structures.Stack;
  * @author rlc
  */
 public class CalculaExpressao {
-    
+
     private String v1;
     private String v2;
     private String opl;
     private Stack stackResult;
-    
-    public CalculaExpressao(int stackLength){
-        
+
+    public CalculaExpressao(int stackLength) {
+
         this.stackResult = new Stack(stackLength);
         this.v1 = new String();
         this.v2 = new String();
         this.opl = new String();
     }
-    
-    
-    public String execute(Queue queueofOut){
+
+    public String execute(Queue queueofOut) {
         return Calculates(queueofOut);
-}
-    private String Calculates(Queue queueofOut){
+    }
+
+    private String Calculates(Queue queueofOut) {
         System.out.println("TEST");
 
-        while(!queueofOut.isEmpty())
-        {
-            if(queueofOut.getValue().equals("T") || queueofOut.getValue().equals("F")){
+        while (!queueofOut.isEmpty()) {
+            if (queueofOut.getValue().equals("T") || queueofOut.getValue().equals("F")) {
                 stackResult.push(queueofOut.getValue());
                 System.out.println("Fila de resultado:" + stackResult.getValue());
                 System.out.println("Removido:" + queueofOut.getValue());
                 queueofOut.remove();
             }
             List listaOperadores = Arrays.asList(new String[]{"~", "-", "^", "V", "<"});
-            if(listaOperadores.contains(queueofOut.getValue())){
+            if (listaOperadores.contains(queueofOut.getValue())) {
                 opl = queueofOut.getValue();
                 System.out.println("opl:" + opl);
                 queueofOut.remove();
                 v2 = stackResult.getValue();
                 System.out.println("v2:" + v2);
-                     stackResult.pop();
-                if(!opl.equals("~")){
+                stackResult.pop();
+                if (!opl.equals("~")) {
                     v1 = stackResult.getValue();
                     System.out.println("v1:" + v1);
                     stackResult.pop();
                     stackResult.push(result(v1, opl, v2));
                     System.out.println("Resultado:" + stackResult.getValue());
-                }
-                else{
-                    if(v2.equals("T")){
+                } else {
+                    if (v2.equals("T")) {
                         stackResult.push("F");
                         System.out.println("Resultado:" + stackResult.getValue());
-                    }
-                    else{
+                    } else {
                         stackResult.push("T");
                         System.out.println("Resultado:" + stackResult.getValue());
                     }
@@ -73,40 +70,36 @@ public class CalculaExpressao {
         }
         return stackResult.getValue();
     }
-    
-    private String result(String v1, String opl, String v2){
-            switch (opl){
-                case "^": 
-                    if(v1.equals(v2)){
-                        return "T";
-                    }
-                    else{
-                        return "F";
-                    }
-                case "V": 
-                    if(v1.equals(v2) && v1.equals("F")){
-                        return "F";
-                    }
-                    else{
-                        return "T";
-                    }
-                case "-":
-                    if(v1.equals("T") && v2.equals("F")){
-                        return "F";
-                    }
-                    else{
-                        return "T";
-                    }
-                case "<":
-                    if(v1.equals(v2)){
-                        return "T";
-                    }
-                    else{
-                        return "F";
-                    }
-                    
-            }
+
+    private String result(String v1, String opl, String v2) {
+        switch (opl) {
+            case "^":
+                if (v1.equals(v2) && v1.equals("T")) {
+                    return "T";
+                } else {
+                    return "F";
+                }
+
+            case "V":
+                if (v1.equals(v2) && v1.equals("F")) {
+                    return "F";
+                } else {
+                    return "T";
+                }
+            case "-":
+                if (v1.equals("T") && v2.equals("F")) {
+                    return "F";
+                } else {
+                    return "T";
+                }
+            case "<":
+                if (v1.equals(v2)) {
+                    return "T";
+                } else {
+                    return "F";
+                }
+
+        }
         return null;
     }
 }
-
